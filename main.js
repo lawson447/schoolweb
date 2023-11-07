bc18c9955030f7b32c91a73ae87399a8ff301edf
lawson447 <146630090+lawson447@users.noreply.github.com>
2023-11-07 17:47:49 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+Document.getElementById(‘signUp’).addEventListener(‘click’, function() {
+    Document.getElementById(‘container’).classList.add(‘right-panel-active’);
+  });
+  
+  Document.getElementById(‘signIn’).addEventListener(‘click’, function() {
+    Document.getElementById(‘container’).classList.remove(‘right-panel-active’);
+  });
+  
+  
